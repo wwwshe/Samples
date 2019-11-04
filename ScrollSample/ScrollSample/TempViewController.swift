46e0adca4f6795e89e0a9e17564c783c340021ef
junjungwook <jungwook.jun@yap.net>
2019-11-04 16:29:55 +0900

    update

---
+++ ScrollSample/ScrollSample/TempViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TempViewController.swift
+//  ScrollSample
+//
+//  Created by jungwook on 2019/10/31.
+//  Copyright © 2019 jungwook. All rights reserved.
+//
+
+import UIKit
+
+class TempViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
